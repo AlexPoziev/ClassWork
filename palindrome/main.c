dfe949410934ecf0f148f69f7e7e31334c867980
Aleksey Poziev <poziev24@gmail.com>
2022-11-09 18:02:35 +0300

    feat: add isPalindrome function

---
+++ palindrome/main.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#define MAX_STRING_LENGTH 100
+
+bool isPalindrome(char *string) {
+    unsigned int leftSide = 0;
+    unsigned int rightSide = strlen(string) - 1;
+    // zero length case
+    if (rightSide == -1) {
+        return true;
+    }
+
+    while (leftSide < rightSide) {
+        if (string[leftSide] != string[rightSide]) {
+            return false;
+        }
+
+        ++leftSide;
+        --rightSide;
+    }
+
+    return true;
+}
+
+int main(void) {
+    if (!trueTest() || !falseTest()) {
+        printf("Tests Failed");
+        return -1;
+    }
+
+    printf("Print string with length less than 100: ");
+
+    char *string = calloc(MAX_STRING_LENGTH, sizeof(char));
+    if (string == NULL) {
+        printf("Not enough memory");
+        return 1;
+    }
+
+    scanf("%[^\n]", string);
+
+    printf("It's %s", isPalindrome(string) ? "a palindrome" : "not a palindrome");
+
+    return 0;
+}
