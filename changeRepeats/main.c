e8ff0943db26be0266f2302124db1e3e0fc6e5c8
Aleksey Poziev <poziev24@gmail.com>
2022-11-09 19:09:30 +0300

    test: add tests

--- changeRepeats/main.c
+++ changeRepeats/main.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 
 #define MAX_LENGTH 1000
 
 int changeRepeats(FILE *file, char *string) {
-    if (file == NULL) {
-        return 1;
-    }
     int currentPosition = 0;
 
     char currentSymbol = (char)fgetc(file);
@@ -17,11 +15,13 @@ int changeRepeats(FILE *file, char *string) {
 
     while (!feof(file)) {
         char previousSymbol = currentSymbol;
+
         currentSymbol = (char)fgetc(file);
         if (currentSymbol == -1) {
             if (string[currentPosition - 1] != previousSymbol) {
                 string[currentPosition] = previousSymbol;
             }
+
             break;
         }
         if (currentSymbol != previousSymbol) {
@@ -36,8 +36,27 @@ int changeRepeats(FILE *file, char *string) {
     return 0;
 }
 
+bool correctTest(void) {
+    char test[5] = {0};
+    FILE *file = fopen("test.txt", "r");
+    if (file == NULL) {
+        return false;
+    }
+
+    changeRepeats(file, test);
+    fclose(file);
+
+    return !strcmp(test, "afgba");
+}
+
 int main(void) {
+    if (!correctTest()) {
+        printf("Tests Failed");
+        return -1;
+    }
+
     printf("Print file name with name length less than 20 and which contains less than 1000 symbol: ");
+
     char *fileName = calloc(20, sizeof(char));
     if (fileName == NULL) {
         printf("Not enough memory");
@@ -54,7 +73,7 @@ int main(void) {
 
     free(fileName);
 
-    char* string = calloc(1000, sizeof(char));
+    char* string = calloc(MAX_LENGTH, sizeof(char));
     if (string == NULL) {
         printf("Not enough memory");
         fclose(file);
